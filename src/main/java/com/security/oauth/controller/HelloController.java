8b3dc4dfe38e360b3643a8c80a6d2510cedaa331
Vijay Mohan <vijaymohan@ults.ulccs>
2024-10-01 08:51:44 +0530

    OAuth Integration with Google and Github logins

---
+++ src/main/java/com/security/oauth/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.security.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome!";
+    }
+
+}
